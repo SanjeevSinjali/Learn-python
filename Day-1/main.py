1ecb55778416717b606deb4905ca4922d625cc8a
SanjeevSinjali <tdkzbiru@gmail.com>
2023-12-07 17:44:58 +0545

    added new code

---
+++ Day-1/main.py
@@ -0,0 +1,11 @@
+# int -> 1,2,3
+# float -> 1.2
+# string -> 'lalit' 
+# char -> a,b,c
+# boolean -> True or False
+
+# device = input('which phone do you use ?')
+# print(device)
+first_number = input(" enter first_number")
+second_number = input("enter second_number ")
+print(int(first_number)+int(second_number))
